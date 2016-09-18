ae7ffe9586dace6525ad0596f9ec656111d318ae
Fjodor Vershinin <fjodor@vershinin.net>
2016-09-18 10:43:47 +0300

    Added activity with drawer menu

--- app/src/main/java/ee/ttu/jcollege/LoginActivity.java
+++ app/src/main/java/ee/ttu/jcollege/LoginActivity.java
@@ -3,6 +3,7 @@ package ee.ttu.jcollege;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -332,12 +333,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
 
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+//            if (success) {
+//                finish();
+//            } else {
+//                mPasswordView.setError(getString(R.string.error_incorrect_password));
+//                mPasswordView.requestFocus();
+//            }
         }
 
         @Override
